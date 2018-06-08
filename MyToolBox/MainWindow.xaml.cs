cb886e325778a23585c86b112870de398e7e2f82
bigtruck <xiaoyong_dev@163.com>
2018-06-09 01:23:23 +0800

    增加文件

---
+++ MyToolBox/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyToolBox
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_N329x_SD_Make(object sender, RoutedEventArgs e)
+        {
+            N329x_SD_Make n329X_SD_Make = new N329x_SD_Make();
+            n329X_SD_Make.Show();
+        }
+    }
+}
---
+++ MyToolBox/N329x_SD_Make.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyToolBox
+{
+    /// <summary>
+    /// N329x_SD_Make.xaml 的交互逻辑
+    /// </summary>
+    public partial class N329x_SD_Make : Window
+    {
+        public N329x_SD_Make()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClick_OpenBoot(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClick_OpenApp(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClick_OpenData(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClick_Make(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
